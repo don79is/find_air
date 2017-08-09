a779355a1afe10e14babb1ef3021a9920457d6d8
don79is <don79is@gmail.com>
2017-08-09 22:23:44 +0300

    add edit delete rec buttons

--- resources/views/admin/adminCore.blade.php
+++ resources/views/admin/adminCore.blade.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html lang="EN">
 <head>
-    {{--@include('admin.adminCss')--}}
+    @include('admin.adminCss')
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="UTF-8">
     <title>{{trans('app.title')}}</title>
-    {{--<link href="/css/app.css" rel=stylesheet>--}}
+    <link href="/css/app.css" rel=stylesheet>
 </head>
 <body>
 @include('admin.adminMenu')
@@ -17,5 +17,5 @@
 </body>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-{{--@yield('scripts')--}}
+@yield('scripts')
 </html>
--- resources/views/admin/adminList.blade.php
+++ resources/views/admin/adminList.blade.php
@@ -1,24 +1,74 @@
 @extends('admin.adminCore')
 
 @section('list')
-    <table>
-        <tr>
-            @foreach($list[0] as $key => $value)
-
-                <th> {{$key}}</th>
-            @endforeach
-        </tr>
-
-        @foreach($list as $key=>$record)
+    <div id="list">
+        {{$title}}<br>
+        <a href="{{$rec}}" class="btn btn-primary" role="button">
+            {{trans('app.rec')}}</a>
+        <hr/>
+        @if(sizeof($list)>0)
+        <table class="table">
+            <thead class="thead-inverse">
             <tr>
-                @foreach($record as $key=> $value)
-
-                    <td> {{$value}}</td>
-
+                @foreach($list[0] as $key => $value)
+                    @if(!in_array($key, $ignore))
+                        <th>{{$key}}</th>
+                    @endif
                 @endforeach
+                @if(isset($edit))
+                    <th> {{ trans('app.edit') }}</th>
+                @endif
+                @if(isset($delete))
+                    <th> {{ trans('app.delete') }}</th>
+                @endif
             </tr>
-        @endforeach
+            </thead>
+            @foreach($list as $key => $record)
+                <tr id="{{$record['id']}}">
+                    @foreach($record as $key => $value)
+                        @if(!in_array($key, $ignore))
+                            <td>
+                                {{$value}}
+                            </td>
+                        @endif
+                    @endforeach
 
-    </table>
+                    <td>
+                        <a class="btn btn-info"
+                           href="{{ route($edit, $record['id']) }}">{{ trans('app.edit') }}</a></td>
+                    <td>
+                        <button class="btn btn-danger"
+                                onclick="deleteItem('{{route( $delete, $record['id'])}}')">{{ trans('app.delete') }}</button>
+                    </td>
 
+                </tr>
+            @endforeach
+        </table>
+        @else {{trans('app.no-data')}}
+
+        @endif
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        function deleteItem(route) {
+            $.ajax({
+                url: route,
+                type: 'DELETE',
+                dataType: 'json',
+                success: function (response) {
+                    $('#' + response.id).remove();
+                },
+                error: function () {
+                    alert('ERROR')
+                }
+            });
+        }
+    </script>
 @endsection
