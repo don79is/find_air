b6d1584c31884079f780f63a0169402335074bd1
don79is <don79is@gmail.com>
2017-08-09 22:23:10 +0300

    add side bar route

--- resources/views/admin/adminMenu.blade.php
+++ resources/views/admin/adminMenu.blade.php
@@ -1,7 +1,10 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Donatas Tumanas
- * Date: 2017-08-08
- * Time: 14:04
- */
+<div id="menu">
+    <h2>{{trans('app.admin_side_bar')}}</h2>
+    <ul>
+
+        <li><a href="{{route('app.countries.index')}}">{{trans('app.countries')}}</a></li>
+        <li><a href="{{route('app.airlines.index')}}">{{trans('app.airlines')}}</a></li>
+        <li><a href="{{route('app.airports.index')}}">{{trans('app.airports')}}</a></li>
+        <li><a href="{{route('app.flights.index')}}">{{trans('app.flights')}}</a></li>
+    </ul>
+</div>
